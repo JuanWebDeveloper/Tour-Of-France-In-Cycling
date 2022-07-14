095624eb91f5f94777581d3746acbf43d0235971
Juan Vargas <juan.vargas@sofka.com.co>
2022-07-14 00:26:05 -0500

    [FEAT] Coleccion de equipos
    Se creo la coleccion y el dto para manipular y guardar la informacion de los equipos

---
+++ cycling-api/src/main/java/com/cycling/api/collection/Team.java
@@ -0,0 +1,21 @@
+package com.cycling.api.collection;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collation = "teams")
+public class Team {
+    @Id
+    private String id_team;
+    private String name_team;
+    private String code_team;
+    private String country_team;
+}
---
+++ cycling-api/src/main/java/com/cycling/api/dto/TeamDto.java
@@ -0,0 +1,30 @@
+package com.cycling.api.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TeamDto {
+    @NotEmpty @NotNull
+    private String id_team;
+    @NotEmpty
+    private String name_team;
+    @NotEmpty
+    private String code_team;
+    @NotEmpty
+    private String country_team;
+
+    public TeamDto(String id_team, String name_team, String code_team, String country_team) {
+        this.id_team = id_team;
+        this.name_team = name_team;
+        this.code_team = code_team;
+        this.country_team = country_team;
+    }
+}

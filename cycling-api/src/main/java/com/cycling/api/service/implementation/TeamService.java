b7c512be24ef76a83cd41d27a0c0680c42f287f4
Juan Vargas <juan.vargas@sofka.com.co>
2022-07-15 01:48:40 -0500

    [FEAT] Servicio para crear un equipo
    Se creo el servicio que nos permite crear un equipo

--- cycling-api/src/main/java/com/cycling/api/service/implementation/TeamService.java
+++ cycling-api/src/main/java/com/cycling/api/service/implementation/TeamService.java
@@ -1,9 +1,21 @@
 package com.cycling.api.service.implementation;
+import com.cycling.api.dto.TeamDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Mono;
 
 import com.cycling.api.repository.TeamRepository;
+import com.cycling.api.util.mapper.TeamMapper;
 
 public class TeamService {
+    @Autowired
     private TeamRepository teamRepository;
+    private TeamMapper teamMapper;
 
+    public TeamService() {
+        this.teamMapper = new TeamMapper();
+    }
 
+    public Mono<TeamDto> createTeam(TeamDto teamDto) {
+        return teamRepository.save(teamMapper.fromTeamDtoToTeam().apply(teamDto)).map(teamMapper.fromTeamToTeamDto());
+    }
 }
--- cycling-api/src/main/java/com/cycling/api/util/mapper/TeamMapper.java
+++ cycling-api/src/main/java/com/cycling/api/util/mapper/TeamMapper.java
@@ -5,23 +5,26 @@ import java.util.function.Function;
 import com.cycling.api.collection.Team;
 import com.cycling.api.dto.TeamDto;
 
-
 public class TeamMapper {
-    public Function<Team, TeamDto> fromTeamToTeamDto = team -> {
-        TeamDto teamDto = new TeamDto();
-        teamDto.setId_team(team.getId_team());
-        teamDto.setName_team(team.getName_team());
-        teamDto.setCode_team(team.getCode_team());
-        teamDto.setCountry_team(team.getCountry_team());
-        return teamDto;
-    };
+    public Function<Team, TeamDto> fromTeamToTeamDto() {
+        return  team -> {
+            TeamDto teamDto = new TeamDto();
+            teamDto.setId_team(team.getId_team());
+            teamDto.setName_team(team.getName_team());
+            teamDto.setCode_team(team.getCode_team());
+            teamDto.setCountry_team(team.getCountry_team());
+            return teamDto;
+        };
+    }
 
-    public Function<TeamDto, Team> fromTeamDtoToTeam = teamDto -> {
-        Team team = new Team();
-        team.setId_team(teamDto.getId_team());
-        team.setName_team(teamDto.getName_team());
-        team.setCode_team(teamDto.getCode_team());
-        team.setCountry_team(teamDto.getCountry_team());
-        return team;
-    };
+    public Function<TeamDto, Team> fromTeamDtoToTeam() {
+        return teamDto -> {
+            Team team = new Team();
+            team.setId_team(teamDto.getId_team());
+            team.setName_team(teamDto.getName_team());
+            team.setCode_team(teamDto.getCode_team());
+            team.setCountry_team(teamDto.getCountry_team());
+            return team;
+        };
+    }
 }

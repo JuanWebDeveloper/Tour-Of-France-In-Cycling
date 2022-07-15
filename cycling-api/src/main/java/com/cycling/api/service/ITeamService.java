03d48fa90eb7e589bb8ca342bb53b91078efb716
Juan Vargas <juan.vargas@sofka.com.co>
2022-07-15 01:36:48 -0500

    [FEAT] Mapper para el equipo
    Se creo la clase que se encargara de trabajar con los datos que nos retorne el servicio del equipo

--- cycling-api/src/main/java/com/cycling/api/service/ITeamService.java
+++ cycling-api/src/main/java/com/cycling/api/service/ITeamService.java
@@ -1,5 +1,13 @@
 package com.cycling.api.service;
 
-public interface ITeamService {
+import com.cycling.api.dto.TeamDto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+public interface ITeamService {
+    public Mono<TeamDto> createTeam(TeamDto teamDto);
+    public Flux<TeamDto> getAllTeams();
+    public Mono<TeamDto> getTeamById(String id_team);
+    public Mono<TeamDto> updateTeam(TeamDto teamDto);
+    public Mono<TeamDto> deleteTeam(String id_team);
 }

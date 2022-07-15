1a9d23d748f2dc8af05411374cf66851656e3c62
Juan Vargas <juan.vargas@sofka.com.co>
2022-07-15 04:42:19 -0500

    [FEAT] Servicio para crear un ciclista
    Se creo el servicio que nos permite crear un ciclista

--- cycling-api/src/main/java/com/cycling/api/service/ICyclistService.java
+++ cycling-api/src/main/java/com/cycling/api/service/ICyclistService.java
@@ -1,4 +1,8 @@
 package com.cycling.api.service;
+import reactor.core.publisher.Mono;
+
+import com.cycling.api.dto.CyclistDto;
 
 public interface ICyclistService {
+    public Mono<CyclistDto> createCyclist(CyclistDto cyclistDto);
 }
--- cycling-api/src/main/java/com/cycling/api/service/implementation/CyclistService.java
+++ cycling-api/src/main/java/com/cycling/api/service/implementation/CyclistService.java
@@ -1,7 +1,28 @@
 package com.cycling.api.service.implementation;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.cycling.api.repository.CyclistRepository;
 import com.cycling.api.service.ICyclistService;
+import com.cycling.api.util.mapper.CyclistMapper;
+import com.cycling.api.dto.CyclistDto;
+import reactor.core.publisher.Mono;
 
 public class CyclistService implements ICyclistService {
+    @Autowired
+    private CyclistRepository cyclistRepository;
+    private final CyclistMapper cyclistMapper;
 
+    public CyclistService() {
+        this.cyclistMapper = new CyclistMapper();
+    }
+
+    @Override
+    public Mono<CyclistDto> createCyclist(CyclistDto cyclistDto) {
+        return cyclistRepository.save(cyclistMapper
+                        .fromCyclistDtoToCyclist()
+                        .apply(cyclistDto))
+                .map(cyclistMapper
+                        .fromCyclistToCyclistDto()
+        );
+    }
 }
